b8bbc7afb3fc7354261ed920b893cad65aa65d2a
Erkka Marjakangas <ede@iki.fi>
2013-04-08 11:31:36 +0000

    Pass in the generator

--- visualize.php
+++ visualize.php
@@ -3,11 +3,27 @@
 $postText = trim(file_get_contents('php://input'));
 file_put_contents("/Users/ede/jison/state/post.txt",$postText);
 //exec("");
-exec("/usr/local/bin/dot -Tpng -o result.png post.txt");
 //echo $postText;
 
 //header("Content-Type: image/png");
 //passthru("cat state_dot.png");
 
+switch($_REQUEST["generator"]){
+  case "mscgen":
+    exec("/usr/local/bin/mscgen -Tpng -o result.png post.txt");
+  break;
+  case "actdiag":
+    exec("/usr/local/bin/actdiag -Tpng -a -o result.png post.txt");
+  break;
+  case "blockdiag":
+    exec("/usr/local/bin/blockdiag -Tpng -a -o result.png post.txt");
+  break;
+  case "nwdiag":
+    exec("/usr/local/bin/nwdiag -Tpng -a -o result.png post.txt");
+  break;
+  default:
+    exec("/usr/local/bin/dot -Tpng -o result.png post.txt");
+  break;
+}
 echo "result.png";
 ?>

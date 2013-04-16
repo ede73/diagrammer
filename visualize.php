5c1f85d00961fc9502aca453dc8403c7a28a9ff6
Erkka Marjakangas <ede@iki.fi>
2013-04-16 08:03:35 +0000

    fixed linux path

--- visualize.php
+++ visualize.php
@@ -6,9 +6,23 @@
 #<!-- Prevents caching at the Proxy Server -->
 #<meta http-equiv="Expires" content="0"/>
 
+function getExe($name){
+$PATH="/usr/bin/";
+if (!file_exists($PATH.$name)){
+  $PATH="/usr/local/bin/";
+  if (!file_exists($PATH.$name)){
+   http_response_code(501);
+   return "./".$name;
+  }
+}
+return $PATH.$name;
+}
 //print_r($_SERVER);
 $postText = trim(file_get_contents('php://input'));
-file_put_contents("/Users/ede/jison/state/post.txt",$postText);
+if (FALSE===file_put_contents("./post.txt",$postText)){
+  http_response_code(500);
+  return;
+}
 //exec("");
 //echo $postText;
 
@@ -17,41 +31,42 @@ file_put_contents("/Users/ede/jison/state/post.txt",$postText);
 
 switch($_REQUEST["visualizer"]){
   case "mscgen":
-    exec("/usr/local/bin/mscgen -Tpng -o result.png post.txt");
+   $r=exec(getExe("mscgen")." -Tpng -o result.png post.txt");
   break;
   case "actdiag":
-    exec("/usr/local/bin/actdiag -Tpng -a -o result.png post.txt");
+    $r=exec(getExe("actdiag")." -Tpng -a -o result.png post.txt");
   break;
   case "blockdiag":
-    exec("/usr/local/bin/blockdiag -Tpng -a -o result.png post.txt");
+    $r=exec(getExe("blockdiag")." -Tpng -a -o result.png post.txt");
   break;
   case "nwdiag":
-    exec("/usr/local/bin/nwdiag -Tpng -a -o result.png post.txt");
+    $r=exec(getExe("nwdiag")." -Tpng -a -o result.png post.txt");
   break;
   case "seqdiag":
-    exec("/usr/local/bin/seqdiag -Tpng -a -o result.png post.txt");
+    $r=exec(getExe("seqdiag")." -Tpng -a -o result.png post.txt");
   break;
   case "dot":
-    exec("/usr/local/bin/dot -Tpng -o result.png post.txt");
+    $r=exec(getExe("dot")." -Tpng -o result.png post.txt");
   break;
   case "twopi":
-    exec("/usr/local/bin/twopi -Tpng -o result.png post.txt");
+    $r=exec(getExe("twopi")." -Tpng -o result.png post.txt");
   break;
   case "circo":
-    exec("/usr/local/bin/circo -Tpng -o result.png post.txt");
+    $r=exec(getExe("circo")." -Tpng -o result.png post.txt");
   break;
   case "fdp":
-    exec("/usr/local/bin/fdp -Tpng -o result.png post.txt");
+    $r=exec(getExe("fdp")." -Tpng -o result.png post.txt");
   break;
   case "sfdp":
-    exec("/usr/local/bin/sfdp -Tpng -o result.png post.txt");
+    $r=exec(getExe("sfdp")." -Tpng -o result.png post.txt");
   break;
   case "neato":
-    exec("/usr/local/bin/neato -Tpng -o result.png post.txt");
+    $r=exec(getExe("neato")." -Tpng -o result.png post.txt");
   break;
   default:
-    exec("/usr/local/bin/dot -Tpng -o result.png post.txt");
+    $r=exec(getExe("dot")." -Tpng -o result.png post.txt");
   break;
 }
+file_put_contents("./error.txt",$r);
 echo "result.png";
 ?>

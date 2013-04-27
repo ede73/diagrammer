813e1d199a5ccf30d75d5dcbd1a0b371a0b0a2d9
Erkka Marjakangas <ede@iki.fi>
2013-04-27 17:27:37 +0000

    Added AST generator

---
+++ generators/ast.js
@@ -0,0 +1,44 @@
+function ast(yy) {
+    var depth=0;
+    function indent(msg){
+    	    if (msg.trim()=="")return "";
+    	    var prefix="";
+    	    for(var i=0;i<depth;i++){
+    	    	    prefix+="    ";
+    	    }
+    	    return prefix+msg;
+    }
+    var processANode = function(o) {
+    };
+    
+    var r = getGraphRoot(yy);
+    yy.result(indent(JSON.stringify({visualizer:r.getVisualizer()})));
+    yy.result(indent(JSON.stringify({direction:r.getDirection()})));
+    yy.result(indent(JSON.stringify({start:r.getStart()})));
+    yy.result(indent(JSON.stringify({equal:r.getEqual()})));
+
+    var traverseObjects = function traverseObjects(r) {
+        for (var i in r.OBJECTS) {
+            var o = r.OBJECTS[i];
+            if (o instanceof Group) {
+                var processAGroup = function(o) {
+			var n = JSON.parse(JSON.stringify(o));
+			n.OBJECTS=undefined;
+                    yy.result(indent(JSON.stringify({group:n})));
+                    depth++;
+                    traverseObjects(o);
+                    depth--;
+                }(o);
+            } else if (o instanceof Node) {
+		yy.result(indent(JSON.stringify({node:o})));
+            } else {
+                throw new Error("Not a node nor a group, NOT SUPPORTED");
+            }
+        }
+    }(r);
+
+    for (var i in yy.LINKS) {
+        var l = yy.LINKS[i];
+	yy.result(indent(JSON.stringify(l)));
+    }
+}

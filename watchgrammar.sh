743b194eda73df588a12ac89c93bf459f0c72a73
Erkka Marjakangas <ede@iki.fi>
2013-04-30 20:00:48 +0000

    While making parser..run the tests as well

--- watchgrammar.sh
+++ watchgrammar.sh
@@ -9,11 +9,15 @@ stat -f"%N %m%b%z%v" $FILE>$old
 while [ 1 ]; do
   stat -f"%N %m%b%z%v" $FILE>$new
   rb=0
-  for c in `cat $new $old|sort|uniq -u|cut -d" " -f1`;do	
+  for c in `cat $new $old|sort|uniq -u|cut -d" " -f1`;do
+   echo $c changed...fire runer
    rb=1
   done
-  [[ $rb -eq 1 ]] &&  ./makeLexerAndParser.sh ; sleep 1
+  [[ $rb -eq 1 ]] &&  {
+	./makeLexerAndParser.sh 
+	./runtests.sh
+  }
   cp $new $old
   rm -f $new
-  sleep 1
+  sleep 2
 done

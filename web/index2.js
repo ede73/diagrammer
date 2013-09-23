abe211090490671c30d17bf78acd030e8a9b1cc7
Erkka Marjakangas <ede@iki.fi>
2013-09-23 21:44:59 +0300

    Add ast/url, setup ace, refactor all direct text area access to thru get/setText appendLine/prependLine. Try to setup and maintain old text area onKeyUp handlers as well as ace on change

--- web/index2.js
+++ web/index2.js
@@ -4,42 +4,93 @@ result = document.getElementById("result");
 var vtimer = null;
 var vdelay = 1000;
 
+//Set to 0 to fall back to textarea(enable textarea in index.html)
+var acemode=1;
+
+//get all
+function getText(){
+  if (acemode){
+    return editor.getSession().getValue();
+  }else{
+    return document.getElementById("editable").value;
+  }
+}
+
+//replace all
+function setText(data){
+  if (acemode){
+    editor.destroy();
+    editor.insert(data);
+  }else{
+    document.getElementById("editable").value = data;
+  }
+}
+//Add text to top of document
+//try to maintain cursor position(TODO:fucked up)
+function addTop(data){
+  if (acemode){
+    //using ace
+    var cursor=editor.getCursorPosition();
+    editor.navigateFileStart();
+    editor.insert(data);
+    //should roughly
+    editor.getSession().getSelection().selectionLead.setPosition(cursor.column,cursor.row-data.split("\n").length+1);
+  }else{
+    var comp = document.getElementById("editable");
+    //using textarea
+    comp.value=data+comp.value;
+  }
+}
+//add text to current cursor position(on a new line how ever)
+function appendLine(comp,data){
+  if (acemode){
+    //using ace insert text into wherever the cursor is pointing.
+    editor.navigateLineEnd();
+    //TODO: If this is empty line, no need for linefeed
+    var cursor=editor.getCursorPosition();
+    if (cursor.column>1)
+      editor.insert("\n");
+    editor.insert(data.trim());
+  }else{
+    //using textarea
+    comp.value=comp.value+data;
+  }
+}
 function addLine(i) {
-    var d = document.getElementById("editable");
     if (typeof i == "string") {
-        d.value = d.value + i + "\n";
+        appendLine(i + "\n");
     } else
         switch (i) {
             case 1:
-                d.value = d.value + "node#ff0000;Label here\n";
+                appendLine("node#ff0000;Label here\n");
                 break;
             case 2:
-                d.value = d.value + "group color #7722ee\ngroup NAME;Label the group\n//Nodes\n  group InnerGroup#00ff00;Inner group\n  xy\n  group end\ngroup end\n";
+                appendLine("group color #7722ee\ngroup NAME;Label the group\n//Nodes\n  group InnerGroup#00ff00;Inner group\n  xy\n  group end\ngroup end\n");
                 break;
             case 3:
-                d.value = d.value + "x#ff0000>#00ff00y#0000ff\n";
+                appendLine("x#ff0000>#00ff00y#0000ff\n");
                 break;
             case 4:
-                d.value = d.value + "a/barcode.png,b/basestation.png,c/battery.png>d/camera.png,e/cpu.png,f/documents.png\n" + "a1/harddisk.png,b1/keyboard.png,c1/laptop.png>d1/laser.png,e1/monitor.png,f1/mouse.png\n" + "a2/phone.png,b2/printer.png,c2/ram.png>d2/satellite.png,e2/scanner.png,f2/sim.png\n" + "u/usbmemory.png>w/wifi.png\n" + "a1/actor1.png>a2/actor2.png>a3/actor3.png";
+                appendLine("a/barcode.png,b/basestation.png,c/battery.png>d/camera.png,e/cpu.png,f/documents.png\n" + "a1/harddisk.png,b1/keyboard.png,c1/laptop.png>d1/laser.png,e1/monitor.png,f1/mouse.png\n" + "a2/phone.png,b2/printer.png,c2/ram.png>d2/satellite.png,e2/scanner.png,f2/sim.png\n" + "u/usbmemory.png>w/wifi.png\n" + "a1/actor1.png>a2/actor2.png>a3/actor3.png");
                 break;
             case 5:
-                d.value = "start NODENAME\n" + d.value;
+                addTop("start NODENAME\n");
                 break;
             case 6:
-                d.value = d.value + "//shapes: default, invis, record, dots, actor, cloud\n" + "//beginpoint,endpoint,condition,database,terminator,input,loopin,loopout\n" + "//square,ellipse,diamond,minidiamond,note,mail\n" + "shape box\n";
+                appendLine("//shapes: default, invis, record, dots, actor, cloud\n" + "//beginpoint,endpoint,condition,database,terminator,input,loopin,loopout\n" + "//square,ellipse,diamond,minidiamond,note,mail\n" + "shape box\n");
                 break;
             case 7:
-                d.value = "equal node1,node2\n" + d.value;
+                addTop("equal node1,node2\n");
                 break;
             case 8:
-                d.value = "$(color1:#12ede0)\nclr$(color1)\nclr2$(color1)\n" + d.value;
+                addTop("$(color1:#12ede0)\nclr$(color1)\nclr2$(color1)\n");
                 break;
             case 9:
-                d.value = "if something would happend then\n" + "  a1>b1\n" + "elseif something probably would not happen then\n" + " a2>b2\n" + "elseif or if i see a flying bird then\n" + " a3>b3\n" + "else\n" + "  a4>b4\n" + "endif\n" + d.value;
+                appendLine("if something would happend then\n" + "  a1>b1\n" + "elseif something probably would not happen then\n" + " a2>b2\n" + "elseif or if i see a flying bird then\n" + " a3>b3\n" + "else\n" + "  a4>b4\n" + "endif\n");
                 break;
     }
     console.log("getSavedFilesChanged..parse");
-    parse(document.getElementById("editable"), getGenerator());
+    parse(getText()+"\n", getGenerator());
     return false;
 }
 
@@ -47,6 +98,7 @@ var win;
 function openPicWindow(){
   win=window.open('web/result.png','extpic');
 }
+
 function reloadImg(id) {
     var obj = document.getElementById(id);
     var src = obj.src;
@@ -60,8 +112,8 @@ function reloadImg(id) {
 	win.location.reload();
     return false;
 }
-// Get currently selected generator
 
+// Get currently selected generator
 function getGenerator() {
     var e = document.getElementById("generator");
     var gen = e.options[e.selectedIndex].value;
@@ -87,6 +139,7 @@ function cancelVTimer() {
     }
 }
 
+/*
 function highlight(tc) {
     var s = document.getElementById("editable");
     s.innerHTML = tc.replace("->", "->>").replace(".>", ".>>").replace("<-",
@@ -98,8 +151,10 @@ function highlight(tc) {
         '<text id="event">&gt;</text>').replace("<<",
         '<text id="event">&lt;</text>');
 }
+*/
 
-function parse(textArea, generator, visualizer) {
+function parse(generator, visualizer) {
+    var data=getText()+"\n";
     console.log("parse " + generator + "," + visualizer);
     document.getElementById("error").innerText = "";
     parsingStarted = true;
@@ -109,7 +164,7 @@ function parse(textArea, generator, visualizer) {
     parser.yy.OUTPUT = generator;
     parser.yy.VISUALIZER = visualizer;
     console.log("Parse, set generator to " + parser.yy.OUTPUT + " visualizer to " + parser.yy.VISUALIZER);
-    parser.parse(textArea.value + "\n");
+    parser.parse(data);
     /*
      * var tc=textArea.textContent; parser.parse(tc+"\n"); highlight(tc);
      */
@@ -123,7 +178,7 @@ function parse(textArea, generator, visualizer) {
 
 function generatorChanged() {
     console.log("generatorChanged..parse");
-    parse(document.getElementById("editable"), getGenerator(), getVisualizer());
+    parse(getGenerator(), getVisualizer());
 }
 
 function savedChanged() {
@@ -133,10 +188,10 @@ function savedChanged() {
     var filename = document.getElementById("filename");
     var data = getSavedGraph();
     if (data[doc]) {
-        document.getElementById("editable").value = data[doc];
+        setText(data[doc]);
         filename.value = doc;
         console.log("savedChanged..parse");
-        parse(document.getElementById("editable"));
+        parse();
     }
 }
 
@@ -148,27 +203,53 @@ function exampleChanged() {
         url: "tests/" + doc,
         cache: false
     }).done(function(data) {
-        document.getElementById("editable").value = data;
+	setText(data);
         console.log("exampleChanged..parse");
-        parse(document.getElementById("editable"));
+        parse();
     });
 }
 
-function delayedKeypressCallback(obj, callback, delay) {
+function textAreaOnChange(callback, delay) {
     var timer = null;
-    var tt = obj;
-    obj.onkeyup = function() { // onchange does not work on
+    document.getElementById("editable").onkeyup = function() { // onchange does not work on
         // chrome/mac(elsewhere?)
         if (timer) {
             window.clearTimeout(timer);
         }
         timer = window.setTimeout(function() {
             timer = null;
-            callback(tt, getGenerator(), getVisualizer());
+            callback(getGenerator(), getVisualizer());
         }, delay);
     };
     obj = null;
 }
-
-delayedKeypressCallback(document.getElementById("editable"), parse, 150);
-delayedKeypressCallback(document.getElementById("result"), visualize, 250);
+function visualizeOnChange(callback, delay) {
+    var timer = null;
+    var tt=document.getElementById("result");
+    tt.onkeyup = function() { // onchange does not work on
+        // chrome/mac(elsewhere?)
+        if (timer) {
+            window.clearTimeout(timer);
+        }
+        timer = window.setTimeout(function() {
+            timer = null;
+            callback(tt,getGenerator(), getVisualizer());
+        }, delay);
+    };
+    obj = null;
+}
+textAreaOnChange( parse, 150);
+visualizeOnChange( visualize, 250);
+if (acemode){
+  var timer2 = null;
+  editor.getSession().on('change', function(){
+    // chrome/mac(elsewhere?)
+    if (timer2) {
+        window.clearTimeout(timer2);
+    }
+    timer = window.setTimeout(function() {
+        timer2 = null;
+        parse(getGenerator(), getVisualizer());
+    }, delay);
+  });
+}

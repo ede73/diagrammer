d74adb4977d5145e16417973d68c7c3fd654656b
Erkka Marjakangas <ede@iki.fi>
2013-12-07 23:16:09 +0200

    auto watch models and generators

--- watchgrammar.sh
+++ watchgrammar.sh
@@ -1,8 +1,17 @@
 old=.$$.old
 new=.$$.new
+GENERATORS=""
+for f in generators/*js;do
+ GENERATORS=$GENERATORS" "$f
+done
+MODELS=""
+for f in model/*js;do
+ MODELS=$MODELS" "$f
+done
+
 FILE="state.lex state.grammar \
-model/support.js model/model.js \
-generators/seqdiag.js generators/mscgen.js generators/blockdiag.js generators/actdiag.js generators/digraph.js generators/nwdiag.js generators/plantuml_sequence.js generators/ast.js"
+$MODELS \
+$GENERATORS"
 
 touch $FILE
 stat -f"%N %m%b%z%v" $FILE>$old

c75d54ab1d4d7c59acf98524dc1277e1d9549340
Erkka Marjakangas <ede@iki.fi>
2021-10-13 00:44:41 +0100

    filenamefix

--- web/saveExport.php
+++ web/saveExport.php
@@ -1,8 +1,19 @@
 <?
 //print_r($_SERVER);
+
+function sanitize() {
+}
+
+function get_safe_ip() {
+  $ip = $_SERVER['REMOTE_ADDR'];
+  return str_replace($ip, array(':'), array('_'));
+}
+
+$ip = get_safe_ip();
+
 $postText = trim(file_get_contents('php://input'));
-if (FALSE===file_put_contents("./localstorage_".$_SERVER['REMOTE_ADDR'].".json",$postText)){
-http_response_code(500);
-return;
+
+if (FALSE===file_put_contents("./localstorage_{$ip}.json", $postText)) {
+  http_response_code(500);
 }
 ?>

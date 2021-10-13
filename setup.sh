0ebbd4b6b966b3be00497b35a832ae64ecbfa911
Erkka Marjakangas <ede@iki.fi>
2021-10-13 01:22:48 +0100

    x

--- setup.sh
+++ setup.sh
@@ -12,5 +12,32 @@ install() {
   exit 10
 }
 
-install jison
+apache_enable_mod() {
+  which a2enmod && {
+    sudo a2enmod $*
+    return 0
+  }
+  echo "Unknown architecture - no a2enmod command"
+  exit 10
+}
+
+apache_restart() {
+ which systemctl && {
+   sudo systemctl restart apache2
+   return 0
+ }
+  echo "Unknown architecture - no systemctl command"
+  exit 10
+}
+
+make_home_dir() {
+  # in linux
+  mkdir ~/public_html
+}
+
+install jison apache2 libapache2-mod-php7.3
+apache_enable_mod userdir
+apache_enable_mod php7
+make_home_dir
+apache_restart
 

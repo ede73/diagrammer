1a86287f41003d5344c5d6f837afe7132fd9d5e3
Erkka Marjakangas <ede@fb.com>
2021-10-26 21:03:13 -0700

    delete coffee

--- model/coffee/update.sh
+++
@@ -1,9 +0,0 @@
-js2coffee ../graphobject.js>graphobject.coffee
-js2coffee ../graphroot.js>graphroot.coffee
-js2coffee ../group.js>group.coffee
-js2coffee ../link.js>link.coffee
-js2coffee ../model.js>model.coffee
-js2coffee ../node.js>node.coffee
-js2coffee ../shapes.js>shapes.coffee
-js2coffee ../support.js>support.coffee
-

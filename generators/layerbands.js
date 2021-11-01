1f3ee1f58ab8a1f335ad4c9494f76e3ab08eeb1e
Erkka Marjakangas <ede@iki.fi>
2021-10-31 19:37:26 -0700

    first draft layers

--- generators/layerbands.js
+++ generators/layerbands.js
@@ -2,68 +2,58 @@
 
 Only one root supported?
 { // this is the information needed for the headers of the bands
-          key: "_BANDS",
-          category: "Bands",
-          itemArray: [
-            { text: "Zero" },
-            { text: "One" },
-            { text: "Two" },
-            { text: "Three" },
-            { text: "Four" },
-            { text: "Five" }
-          ]
-        },
-        // these are the regular nodes in the TreeModel
-        { key: "root" },
-        { key: "oneB", parent: "root" },
-        { key: "twoA", parent: "oneB" },
-        { key: "twoC", parent: "root" },
-        { key: "threeC", parent: "twoC" },
-        { key: "threeD", parent: "twoC" },
-        { key: "fourB", parent: "threeD" },
-        { key: "fourC", parent: "twoC" },
-        { key: "fourD", parent: "fourB" },
-        { key: "twoD", parent: "root" }
-      ];
+		  key: "_BANDS",
+		  category: "Bands",
+		  itemArray: [
+			{ text: "Zero" },
+			{ text: "One" },
+			{ text: "Two" },
+			{ text: "Three" },
+			{ text: "Four" },
+			{ text: "Five" }
+		  ]
+		},
+		// these are the regular nodes in the TreeModel
+		{ key: "root" },
+		{ key: "oneB", parent: "root" },
+		{ key: "twoA", parent: "oneB" },
+		{ key: "twoC", parent: "root" },
+		{ key: "threeC", parent: "twoC" },
+		{ key: "threeD", parent: "twoC" },
+		{ key: "fourB", parent: "threeD" },
+		{ key: "fourC", parent: "twoC" },
+		{ key: "fourD", parent: "fourB" },
+		{ key: "twoD", parent: "root" }
+	  ];
+	  node parse.js verbose layerbands.test layerbands
+
 */
 
 function generateLayerBands(yy) {
-	var nodeList = [];
-	function addLinkedNode(left, right) {
-		if (!(left instanceof Node)) return;
-		if (!(right instanceof Node)) return;
-		const key=right.id;
-		const parent=left.id;
-		const text=(right.label == undefined) ? right.name : right.label;
-		nodeList.push({key: key, text: text, fill: "#f8f8f8", stroke: "#4d90fe", parent: parent});
-	}
+	//console.log(yy)
+	const groups = {
+		key: "_BANDS",
+		category: "Bands",
+		itemArray: [],
+	};
+	const linkedNodes = [
+		groups,
+	]
+	const root = getGraphRoot(yy);
 
-	//console.log(JSON.stringify(yy.LINKS));
-
-	const root = getGraphRoot(yy).ROOTNODES;
-	if (root.length > 1) {
-		throw new Error('Only one root node supported');
-	}
-
-	(() => {
-		root[0].id = 1;
-		const text=(root[0].label == undefined) ? root[0].name : root[0].label;
-		nodeList.push({key: root[0].id, text: text, fill: "#f8f8f8", stroke: "#4d90fe"});
-		var keyId = 2;
-		getGraphRoot(yy).OBJECTS.forEach((node) => {
-			if (!getAttr(node, 'id')) {
-				node.id = keyId++;
-			}
-		});
-	})();
-
-	//console.log(JSON.stringify(getGraphRoot(yy).OBJECTS));
+	traverseObjects(root, o => {
+		if (o instanceof Group) {
+			groups.itemArray.push({ text: o.name });
+		}
+	});
 
 	traverseLinks(yy, l => {
-		debug('link node '+l.left.name+' to '+l.right.name);
-		addLinkedNode(l.left, l.right);
+		if (!l.left) {
+			// probably our root
+			//linkedNodes.push({key: l.right.name});
+		}else{
+			linkedNodes.push({key: l.right.name , parent: l.left.name});
+		}
 	});
-	//console.log(JSON.stringify(nodeList));
-	output(yy, JSON.stringify(nodeList));
-	output(false);
+	output(yy, JSON.stringify(linkedNodes));
 }

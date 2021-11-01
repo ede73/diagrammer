ae55eda9778403e125f98e2612ff5fec3dd8184b
Erkka Marjakangas <ede@iki.fi>
2021-10-31 21:27:51 -0700

    first draft uml

---
+++ generators/umlclass.js
@@ -0,0 +1,71 @@
+/*
+
+	  // setup a few example class nodes and relationships
+	  var nodedata = [
+		{
+		  key: 1,
+		  name: "BankAccount",
+		  properties: [
+			{ name: "owner", type: "String", visibility: "public" },
+			{ name: "balance", type: "Currency", visibility: "public", default: "0" }
+		  ],
+		  methods: [
+			{ name: "deposit", parameters: [{ name: "amount", type: "Currency" }], visibility: "public" },
+			{ name: "withdraw", parameters: [{ name: "amount", type: "Currency" }], visibility: "public" }
+		  ]
+		},
+
+	  var linkdata = [
+		{ from: 12, to: 11, relationship: "generalization" },
+		{ from: 13, to: 11, relationship: "generalization" },
+		{ from: 14, to: 13, relationship: "aggregation" }
+	  ];
+
+
+	  node parse.js verbose uml.test umlclass
+
+*/
+
+function generateUmlClass(yy) {
+	//console.log(yy)
+	const groups = [];
+	const links = [];
+	const root = getGraphRoot(yy);
+
+	String.prototype.endsWith = function(suffix) {
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+	const labelOrName = ln => {
+		return (ln.label == undefined) ? ln.name : ln.label;
+	}
+
+	const getProperties = nodes => {
+		return [...nodes].filter(node => !labelOrName(node).endsWith(")"));
+	};
+	const getMethods = nodes => {
+		return [...nodes].filter(node => labelOrName(node).endsWith(")"));
+	};
+	var id = 1;
+	traverseObjects(root, o => {
+		if (o instanceof Group) {
+			console.log(o);
+			const key = id++;
+			groups.push({
+				key: key,
+				name: labelOrName(o),
+				properties: getProperties(o.OBJECTS),
+				methods: getMethods(o.OBJECTS)
+			});
+		}
+	});
+
+	traverseLinks(yy, l => {
+		relationship = 'aggregation';
+		links.push({
+			from: l.left.id,
+			to: l.right.id,
+			relationship: relationship
+		});
+	});
+	output(yy, JSON.stringify([groups, links]));
+}

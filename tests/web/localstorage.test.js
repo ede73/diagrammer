f3a90e1a96f4395c89482c1bc565a60c8bcd8f7e
Erkka Marjakangas <ede@iki.fi>
2023-02-13 22:40:56 -0800

    import test also

--- tests/web/localstorage.test.js
+++ tests/web/localstorage.test.js
@@ -11,6 +11,13 @@ describe('Diagrammer', () => {
     beforeAll(async () => {
         await page.goto('http://localhost/~ede/diagrammer/');
         await page.setViewport({ width: 1800, height: 1800 });
+        await page.evaluate((localStorageKey) => {
+            localStorage.clear(localStorageKey);
+        }, localStorageKey);
+        page.on('dialog', async dialog => {
+            console.log(`Alert dialog displayed(${dialog.message()}), dismiss it`);
+            await dialog.dismiss();
+        });
         // await captureBrowserLogs(page);
     });
 
@@ -33,5 +40,35 @@ describe('Diagrammer', () => {
         // CSS selector for puppeteer
         await page.click('form[name="contact"]>button#loadfile');
         expect(getDiagrammerCode(page)).resolves.toMatch("this>is>localstorage>test");
+    });
+
+    // Initially wanted these to be in separare file, but tests executed in paraller, so messing up local storage (between these two tests)
+    it('Import from external storage (mocked)', async () => {
+        await page.click('form[name="contact"]>button#import');
+
+        // TODO: mock out
+        // Import is asynchronous, it'll take a moment for results to arrive
+        await sleepABit(500);
+
+        const storedGraphs = await page.evaluate((localStorageKey) => {
+            return localStorage.getItem(localStorageKey);
+        }, localStorageKey);
+        await sleepABit(2000);
+        expect(storedGraphs).toMatch("{\"joo\":\"a>b>c>d\\n\"}");
+    });
+
+    // Only way to know export worked (in RL) would be to change smthg, export, import
+    // This basically tests backend integration which is bad
+    // Would be enough to test button to export route
+    it.skip('Export to external storage (mocked)', async () => {
+        // TODO:
+        //await captureBrowserLogs(page);
+        await page.click('form[name="contact"]>button#export');
+
+        // TODO: mock out
+        // Import is asynchronous, it'll take a moment for results to arrive
+        await sleepABit(500);
+
+        expect(true).toBeTruthy();
     });
 });

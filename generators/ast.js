e8962c3c2d368219e8a2da0e20553049481b5abd
Erkka Marjakangas <ede@iki.fi>
2023-02-19 03:31:18 -0800

    stop infinity, CURRENTCONTAINER is selfreferential, dont visualize in ast fully(for now)

--- generators/ast.js
+++ generators/ast.js
@@ -25,9 +25,9 @@ export function ast (graphcanvas) {
   }
 
   const dumpObject = (obj, level = 0) => {
-    // if (level > 3) {
-    //   return
-    // }
+    if (level > 50) {
+      return `"/* Stopping recursion, already ${level}s deep and going ${obj}*/"`
+    }
     // const t = typeof (obj)
     // const a = Array.isArray(obj)
     // const c = (obj && typeof (obj.constructor) !== 'undefined') ? obj.constructor.name : ''
@@ -60,6 +60,9 @@ export function ast (graphcanvas) {
           propname === 'container') {
           // special case for GraphEdges..don't traverse into these!
           items.push(`\n"${propname}": ${rvalue(value.getName())}`)
+        } else if (propname === 'CURRENTCONTAINER') {
+          // infinity awaits..just dump the name
+          items.push(`\n"${propname}": "...${value}"`)
         } else if (propname === 'yy' ||
           propname === 'ALLOWED_DEFAULTS' ||
           typeof (value) === 'function') {

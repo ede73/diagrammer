37e90d081ee3f84169ba476afaf6065a8bcb2ca1
Erkka Marjakangas <ede@iki.fi>
2023-03-11 01:05:52 -0800

    add web visualizations to watched automake

--- scripts/watch_and_make.sh
+++ scripts/watch_and_make.sh
@@ -52,7 +52,7 @@ isBuildNeeded() {
 #if isBuildNeeded; then echo build needed; fi
 #if ! isBuildNeeded; then echo build not needed; fi
 
-inotifywait -m -e modify --format '%w%f' --include "(.*.html|.*.ts)$" index_template.html model generators web tests/web tests/parser tests/generators tests/model |
+inotifywait -m -e modify --format '%w%f' --include "(.*.html|.*.ts)$" index_template.html model generators web tests/web tests/parser tests/generators tests/model web/visualizations |
     while read path; do
         touch "$WATCHED_FILES_CHANGED"
         #maybeBuild

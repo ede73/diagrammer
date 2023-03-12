4c1ab4c3df1eea3d3a62486e80223e7973178d53
Erkka Marjakangas <ede@iki.fi>
2023-03-11 23:57:51 -0800

    Add better tree reconstruction method for dendrograms. Not fully done, doesnt support cyclical nodes, but it can now perfectly (almost :) ) construct connected trees from edges (ie. it only sees linke between two nodes, but can still make up the tree). Tree can have multiple roots. All the root are returned. GraphInner causes some problems (and bi/uni directional edges as well)

--- generators/dendrogram.ts
+++ generators/dendrogram.ts
@@ -2,11 +2,8 @@
 
 // WEB VISUALIZER ONLY -- DO NOT REMOVE - USE IN AUTOMATED TEST RECOGNITION
 import { generators, type GraphCanvas, visualizations } from '../model/graphcanvas.js'
-import { GraphVertex } from '../model/graphvertex.js'
-import { type GraphConnectable } from '../model/graphconnectable.js'
 import { output } from '../model/support.js'
-import { findVertex, traverseTree, TreeVertex } from '../model/tree.js'
-import { debug } from '../model/debug.js'
+import { makeConnectedTree, traverseTree } from '../model/tree.js'
 
 // ADD TO INDEX.HTML AS: <option value="dendrogram:radialdendrogram">Radial Dendrogram</option>
 // ADD TO INDEX.HTML AS: <option value="dendrogram:reingoldtilford">Reingold-Tilford</option>
@@ -20,28 +17,6 @@ export interface DendrogramDocument {
 
 /**
  * To test: node js/generate.js verbose tests/test_inputs/dendrogram.txt dendrogram
- * TODO: The class hierarchy is disconnected. Just using getObjects() doesn't allow proper graph traversal
- *
- * There's nothing wrong with the model - all are recorded (check ast/ast_record) but especially for dendrogram
- *
- * It is possible to construct a graph say a>b>c>d c>(k,v) d>(l,o) (or similar) that dendrogram cannot reach all nodes
- *
- * It processes edges first, first LHS becomes tree root (dendrogram can have 1 root only) and all
- * reminder nodes are added subsequently.
- *
- * Some of the nodes are just GraphReferences (if seen few times), problem #1 - mitigated partially
- * Then it seems like GraphInners werent includes (they're GraphGroups, but dendro generation missed this) - mitigated
- * How ever the order the link traversal sees the nodes, the LHS GraphVertex may be MISSED in traversal
- *
- * Now, taking GraphCanvas, its objects, we only see a,b,c,d..root notes, not the inners
- * One would have to traverse deeper.
- *
- * Anway, doable, but defeates the purposes hacking around this inefficiency in the generator.
- *
- * GraphCanvas (or traversal/support) should provide this. Perhaps also instead of naive object
- * storage, we could have a proper linked tree there.
- *
- * Long rant, but gotta get fixed, else radial dendrogram visualization is iffy
  */
 export function dendrogram(graphcanvas: GraphCanvas) {
   const lout = (...args: any[]) => {
@@ -49,35 +24,20 @@ export function dendrogram(graphcanvas: GraphCanvas) {
     output(graphcanvas, textOrIndent, maybeIndent)
   }
 
-  let tree: TreeVertex | undefined
-  function addVertex(lhs: GraphConnectable, rhs: GraphConnectable) {
-    if (!tree) {
-      tree = new TreeVertex(lhs)
-    }
-    if (!(lhs instanceof GraphVertex)) return
-    const cl = findVertex(tree, lhs)
-    if (!cl) {
-      throw new Error(`Left node (${lhs.name ?? ''}) not found from tree`)
-    }
-    if (!findVertex(tree, rhs) && (rhs instanceof GraphVertex)) {
-      debug(`Add ${rhs.name ?? ''} as child of ${cl.data.name as string}`)
-      cl.CHILDREN.push(new TreeVertex(rhs))
-    }
-  }
-
-  /**
-   * For a dendrogram we're not interested in vertices
-   * just edges(for now!)
-   */
-  graphcanvas.getEdges().forEach(edge => {
-    addVertex(edge.left, edge.right)
-  })
+  const tree = makeConnectedTree(graphcanvas)
 
   // just for the linter
   if (!tree) {
     throw new Error('No tree')
   }
-  traverseTree(tree, (t, isLeaf, hasSibling) => {
+  if (tree.length !== 1) {
+    tree.forEach(element => {
+      console.warn(element.toString())
+    })
+    throw new Error('Sorry, dendrogram must have exactly one root, no more, no less ')
+  }
+
+  traverseTree(tree[0], (t, isLeaf, hasSibling) => {
     if (isLeaf) {
       let comma = ''
       if (hasSibling) { comma = ',' }
--- model/tree.ts
+++ model/tree.ts
@@ -1,6 +1,9 @@
 // @ts-check
 import { debug } from './debug.js'
-
+import { type GraphConnectable } from './graphconnectable.js'
+import { GraphContainer } from './graphcontainer.js'
+import { type GraphCanvas } from './graphcanvas.js'
+import { GraphEdgeDirectionType } from './graphedge.js'
 /**
  * Tree representration
  */
@@ -12,7 +15,7 @@ export class TreeVertex {
   }
 
   toString() {
-    return `tree(${String(this.data)},children=[${JSON.stringify(this.data)}])`
+    return `tree(${this.data.getName()}, children=[${this.CHILDREN.map(c => c.data.getName()).join(', ')}])`
   }
 };
 
@@ -82,4 +85,90 @@ export function traverseTree(root: TreeVertex,
     debug(`${String(root.data.name)}has sibling`)
     exit(root, hasSibling)
   }
+}
+
+/**
+ * Convert graph canvas to a tree. Does not consider edges
+ *
+ * TODO:  This makes things odd z>(q)>(a b c) will link q to a, b and c, but in model this is GraphInner that holds a,b,c,
+ * so we should fade that a way. Compatible way to present this would be z>q>(a b c), but BOTH should be accepted and produce same results
+ *
+ * @param canvas
+ */
+export function makeEdgeslessTree(node: GraphConnectable, allowReferences: boolean = false): TreeVertex {
+  if (node instanceof GraphContainer) {
+    // go thru all the nodes
+    const subTree = new TreeVertex(node)
+    subTree.CHILDREN = node.getObjects(allowReferences).map((node) => makeEdgeslessTree(node, allowReferences))
+    return subTree
+  } else {
+    // add to tree and return
+    return new TreeVertex(node)
+  }
+}
+
+/**
+ * Construct a connected tree from edges. Alas this MAY result to tree having multiple roots.
+ *
+ * A TreeVertex represents a subtree, and all of its children are connected subtrees
+ *
+ * Currently collects all directional or non-directional edges. Left/Right make perfect one way directions
+ * Bidirectional will be pointing back and forth and so will unidirectional
+ * @param canvas
+ * @param allowReferences if true, allows GraphReferences handled separately
+ * @returns Return all the roots of the connected tree. Notice! Does not support cyclical trees
+ */
+export function makeConnectedTree(canvas: GraphCanvas, allowReferences: boolean = false): TreeVertex[] {
+  const trees: TreeVertex[] = []
+
+  // make a new or find a TreeVertex that contains node
+  function makeSubTree(node: GraphConnectable): TreeVertex {
+    const found = trees.find((p) => p.data === node)
+    if (found) {
+      return found
+    }
+    const newVertex = new TreeVertex(node)
+    trees.push((newVertex))
+    return newVertex
+  }
+
+  canvas.getEdges().forEach(edge => {
+    // we've now an edge from node to node, alas it may be ANY node in the tree, but it DOES produce a subtree
+    switch (edge.direction()) {
+      case GraphEdgeDirectionType.LEFT: {
+        const r = makeSubTree(edge.right)
+        r.CHILDREN.push(makeSubTree(edge.left))
+      }
+        break
+      case GraphEdgeDirectionType.RIGHT: {
+        const r = makeSubTree(edge.left)
+        r.CHILDREN.push(makeSubTree(edge.right))
+      }
+        break
+      case GraphEdgeDirectionType.BIDIRECTIONAL: {
+        const r1 = makeSubTree(edge.left)
+        r1.CHILDREN.push(makeSubTree(edge.right))
+        const r2 = makeSubTree(edge.right)
+        r2.CHILDREN.push(makeSubTree(edge.left))
+      }
+        break
+      case GraphEdgeDirectionType.UNIDIRECTIONAL: {
+        const r1 = makeSubTree(edge.left)
+        r1.CHILDREN.push(makeSubTree(edge.right))
+        const r2 = makeSubTree(edge.right)
+        r2.CHILDREN.push(makeSubTree(edge.left))
+      }
+        break
+    }
+  })
+
+  // since we process the EDGEs, there are no TreeVertices that have no children
+  // Simple way is to remove all TreeVertex's that are someone's children, that leaves root
+  // (if uni-directed non-cyclical tree)
+  const isSomeonesChild = (vertex: TreeVertex) => {
+    return trees.some(p => p.CHILDREN.includes(vertex))
+  }
+
+  const roots = trees.filter(n => !isSomeonesChild(n))
+  return roots
 }

c8408e9791edc4eb3783c0151705c9420ca589a1
Erkka Marjakangas <ede@iki.fi>
2023-03-13 05:28:46 -0700

    add one more test (todo)

--- tests/model/tree.test.ts
+++ tests/model/tree.test.ts
@@ -2,7 +2,7 @@ import { GraphCanvas } from '../../model/graphcanvas.js'
 import { GraphEdge } from '../../model/graphedge.js'
 import { GraphVertex } from '../../model/graphvertex.js'
 import { dendrogram } from '../../generators/dendrogram.js'
-import { makeConnectedTree, type TreeVertex } from '../../model/tree.js'
+import { makeConnectedTree, type TreeVertex, traverseTree } from '../../model/tree.js'
 
 const findNode = (name: string, canvas: GraphCanvas): GraphVertex | undefined => {
   return canvas.getObjects().find(p => p.getName() === name)
@@ -38,8 +38,12 @@ const childList = (subTree: TreeVertex): string => {
   return subTree.CHILDREN.map((c) => c.data.getName()).join(',')
 }
 
+const describeEdge = (edge: GraphEdge) => {
+  return [edge.left.getName(), edge.edgeType, edge.right.getName()]
+}
+
 const edgeList = (subTree: TreeVertex): string => {
-  return subTree.EDGES.map((e) => [e.left.getName(), e.edgeType, e.right.getName()]).join(',')
+  return subTree.EDGES.map((e) => describeEdge(e)).join(',')
 }
 
 it('Ensure making cyclical tree succeeds', async () => {
@@ -86,3 +90,37 @@ it('Ensure making two root tree succeeds', async () => {
   expect(childList(root2)).toMatch('child2')
   expect(edgeList(root2)).toMatch('root2,>,child2')
 })
+
+it('Ensure traversing cyclical tree succeeds', async () => {
+  const tree = makeConnectedTree(simpleCyclicalTree())
+
+  const traversal: string[] = []
+  traverseTree(tree[0], (node, isLeaf, hasSiblings, nthNodeOnTheLevel, edge) => {
+    traversal.push(node.data.getName())
+    traversal.push(isLeaf ? 'leaf' : 'node')
+    traversal.push(hasSiblings ? 'siblings' : 'single')
+    traversal.push(nthNodeOnTheLevel ? 'first' : 'subsequent')
+    if (edge) {
+      traversal.push(describeEdge(edge).join(','))
+    }
+  }, (node) => {
+    traversal.push(node.data.getName())
+  }, (node, hasSiblings) => {
+    traversal.push(node.data.getName())
+    traversal.push(hasSiblings ? 'siblings' : 'single')
+  })
+
+  expect(traversal.join(',')).toBe('a,node,single,subsequent,a,b,node,single,subsequent,a,>,b,b,c,node,single,subsequent,b,>,c,c,c,single,b,single,a,single')
+})
+
+// TODO:
+/*
+// this is a bug in tree traversal (cyclic trees)
+/*
+ROOT > FirstRootLeaf1 > ( SecondLeaf > ROOT > FirstRootLeaf2 )
+
+ROOT            has children (recursive list w/ lelvel) 1 - FirstRootLeaf1, 1 - FirstRootLeaf2, 2 - SecondLeaf
+FirstRootLeaf1  has children 1 - ROOT, 2 - SecondLeaf
+SecondLeaf      has children 1 - ROOT
+FirstRootLeaf2  has no children
+*/
